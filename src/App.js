a1413ef72bb3fa1f6bb4d0232fc7c1aca9d1de85
jisooyu <jisooyu@hotmail.com>
2022-02-02 20:21:20 +0900

    NavLink

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  NavLink,
+} from 'react-router-dom';
 import { v4 as uuidv4 } from 'uuid';
 import Header from './components/Header';
 import AboutPage from './components/pages/AboutPage';
@@ -8,6 +13,7 @@ import FeedbackList from './components/FeedbackList';
 import FeedbackStats from './components/FeedbackStats';
 import FeedbackForm from './components/FeedbackForm';
 import AboutIconLink from './components/AboutIconLink';
+import Card from './components/shared/Card';
 
 function App() {
   const [feedback, setFeedback] = useState(FeedbackData);
@@ -38,6 +44,14 @@ function App() {
                   feedback={feedback}
                   handleDelete={deleteFeedback}
                 />
+                <Card>
+                  <NavLink to='/' activeClassName='active'>
+                    Home
+                  </NavLink>
+                  <NavLink to='/about' activeClassName='active'>
+                    About
+                  </NavLink>
+                </Card>
                 <AboutIconLink />
               </>
             }></Route>
