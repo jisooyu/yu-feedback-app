39e44f4d26a2e5512eb3c923754d7f75b137b561
jisooyu <jisooyu@hotmail.com>
2022-02-03 16:54:09 +0900

    useParams, useNavigate, nested navigation

--- src/App.js
+++ src/App.js
@@ -1,19 +1,14 @@
 import { useState } from 'react';
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  NavLink,
-} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { v4 as uuidv4 } from 'uuid';
 import Header from './components/Header';
 import AboutPage from './components/pages/AboutPage';
+import Post from './components/Post';
 import FeedbackData from './data/FeedbackData';
 import FeedbackList from './components/FeedbackList';
 import FeedbackStats from './components/FeedbackStats';
 import FeedbackForm from './components/FeedbackForm';
 import AboutIconLink from './components/AboutIconLink';
-import Card from './components/shared/Card';
 
 function App() {
   const [feedback, setFeedback] = useState(FeedbackData);
@@ -44,18 +39,11 @@ function App() {
                   feedback={feedback}
                   handleDelete={deleteFeedback}
                 />
-                <Card>
-                  <NavLink to='/' activeClassName='active'>
-                    Home
-                  </NavLink>
-                  <NavLink to='/about' activeClassName='active'>
-                    About
-                  </NavLink>
-                </Card>
                 <AboutIconLink />
               </>
             }></Route>
           <Route path='/about' element={<AboutPage />} />
+          <Route path='/post/*' element={<Post />} />
         </Routes>
       </div>
     </Router>
