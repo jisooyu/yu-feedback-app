08919fb13415828d07f1b94897e7472d7639c3e0
jisooyu <jisooyu@hotmail.com>
2022-02-05 16:35:06 +0900

    testing the netlify upload

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -20,6 +20,11 @@ export const FeedbackProvider = ({ children }) => {
       text: 'This is test feedback 3',
       rating: 7,
     },
+    {
+      id: 4,
+      text: 'This is test feedback 4',
+      rating: 5,
+    },
   ]);
 
   const [feedbackEdited, setFeedbackEdited] = useState({
